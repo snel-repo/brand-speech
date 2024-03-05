341655ecf8bbff57b77b6679fde05880b3a57f29
Sam-NT <samRnason@gmail.com>
2024-03-05 17:22:00 -0500

    Updating brand-speech environment setup to include LM and BRAND API

--- install.sh
+++ install.sh
@@ -19,6 +19,22 @@ checkStatus () {
 
 install_script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# install system dependencies
+dependencies=(
+cmake=3.22.1-1ubuntu1.22.04.2
+python3-pyaudio=0.2.11-1.3ubuntu1
+portaudio19-dev=19.6.0-1.1
+)
+
+# install pkgs in $dependencies
+for dep in ${dependencies[@]}; do
+    info "Installing ${dep}"
+    sudo apt-get update
+    sudo apt-get -y install ${dep}
+    checkStatus $? "failed to install ${dep}"
+    info "Successfully installed ${dep}"
+done
+
 # update conda environment
 info "Updating brand-speech conda env"
 conda env update --file $install_script_dir/environment.yaml --prune
